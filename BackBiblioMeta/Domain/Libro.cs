953087bb8dc43c316ebe02b767a0d42ebe7c8492
KevAlex <kevin.alexdiaz@hotmail.com>
2022-08-09 21:09:32 +0200

    Book entity added

---
+++ BackBiblioMeta/Domain/Libro.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+
+        public string Year { get; set; }
+
+        public bool Status { get; set; }
+
+        public string OperationType { get; set; }
+
+    }
+}
