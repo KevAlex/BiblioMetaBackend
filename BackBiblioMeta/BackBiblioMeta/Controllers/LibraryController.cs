ff95994464ef5ff187c45b461d6f21209d50488c
KevAlex <kevin.alexdiaz@hotmail.com>
2022-08-11 00:21:07 +0200

    Comments added

--- BackBiblioMeta/BackBiblioMeta/Controllers/LibraryController.cs
+++ BackBiblioMeta/BackBiblioMeta/Controllers/LibraryController.cs
@@ -17,13 +17,22 @@ namespace BackBiblioMeta.Controllers
             _mediator = mediator;
         }
 
+        /// <summary>
+        /// Obtiene los libros para un determinado alias del usuario
+        /// </summary>
+        /// <param name="alias"> Alias del usuario usado en el registro</param>
+        /// <returns></returns>
         [HttpGet(Name = "getUserBook")]
-        // It should return a UsuarioDTO object list
         public async Task<IEnumerable<ResponseLibroDto>> GetUserBook(string alias)
         {
             return await _mediator.Send(new UserBookQuery { Alias = alias });
         }
 
+        /// <summary>
+        /// Añade un libro a un usuario
+        /// </summary>
+        /// <param name="command">Ver estructura de OperationBookCommand</param>
+        /// <returns></returns>
         [HttpPost(Name = "postBook")]
         public async Task<IActionResult> AddBookUser(OperationBookCommand command)
         {
